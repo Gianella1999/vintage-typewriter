3dfdbcec8fb09e2dd60853784b53ec2f8c872322
Gianella1999 <gianellajiron@gmail.com>
2026-04-09 17:23:12 +0200

    Estructura html y estilos basicos css

--- src/main.js
+++ src/main.js
@@ -1,60 +1,32 @@
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+//create keyboard
+const keyboardLayout = [
+  ['1','2','3','4','5','6','7','8','9','0','-','+'],
+  ['Q','W','E','R','T','Y','U','I','O','P','.', '→'],
+  ['A','S','D','F','G','H','J','K','L',':', '"'],
+  ['Z','X','C','V','B','N','M',';','.','?'],
+  [''] // backspace
+];
 
-<div class="ticks"></div>
+const container = document.querySelector('.typewriter_keyboard');
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src=${viteLogo} alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+  keyboardLayout.forEach(row => {
+    const rowDiv = document.createElement('div');
+    rowDiv.className = 'keys_row';
+    row.forEach(key => {
+      const btn = document.createElement('button');
+      if(key === ''){
+        btn.className = "key-space"
+      }else {
+      btn.className = 'key';
+      btn.textContent = key;
+      }
+      // data-char guarda el valor real (ej: para '⌫' guardamos 'backspace')
+      const charValue = (key === '⌫') ? 'backspace' : key;
+      btn.dataset.char = charValue;
+      rowDiv.appendChild(btn);
+    });
+    container.appendChild(rowDiv);
+  });
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
-
-setupCounter(document.querySelector('#counter'))
